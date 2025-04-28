5b85665e7452ebd9cb4648820e9a629293a5fa3f
studziaash9914 <shaheryaarzia2@gmail.com>
2025-04-28 12:01:09 +0200

    classification test update --- pipeline base

---
+++ Src/OCR_Classification_Model/Model/test_model.py
@@ -0,0 +1,14 @@
+import requests
+import json
+import os
+import numpy as np
+
+
+class TestModel:
+    def test_model(self):
+        # Define the URL for the API endpoint
+        url = "http://localhost:5000/predict"
+
+
+testbot=TestModel()
+testbot.test_model()
