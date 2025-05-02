5686051591abb74cf8eb400c6fea78d50bfcb9d8
studroshal9312 <alina.roshan@stud.th-rosenheim.de>
2025-05-02 22:44:19 +0200

    Configure input/output directories and paths

--- Src/OCR_Classification_Model/scripts/ocr_extraction2.py
+++ Src/OCR_Classification_Model/scripts/ocr_extraction2.py
@@ -12,4 +12,12 @@ from PIL import Image
 from pdf2image import convert_from_bytes
 import pytesseract
 
+# ─── Configuration ──────────────────────────────────────────────────────────────
+IMAGE_DIR   = Path(r"PSM-Protech-Feasibility-Study\Src\OCR_Extraction_Model\Dataset")
+PDF_DIR     = IMAGE_DIR.parent / "pdfs"
+CSV_DIR     = IMAGE_DIR.parent / "csvs"
+OUTPUT_CSV  = CSV_DIR / "extracted_numbers.csv"
+# ────────────────────────────────────────────────────────────────────────────────
+
+
 
