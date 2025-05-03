52254ffed4f17923c4eae52be0f7ddd13493e6fe
studvaikmu5453 <mugesh.vaikundamani@stud.th-rosenheim.de>
2025-05-03 15:51:13 +0200

    Yep

--- Src/OCR_Classification_Model/Dataset/post_annotator/a.py
+++
@@ -1,5 +0,0 @@
-rea=r'C:\Users\Admin\PSE\PSM-Protech-Feasibility-Study\Src\OCR_Classification_Model\post_annotator\form_surface_profile copy 2.png'
-a=rea.find('surface')
-print(a)
-with open(rea,'r') as file:
-    file.write('surface.png')
