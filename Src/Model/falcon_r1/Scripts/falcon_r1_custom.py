87d3dbf1da4933ce184aad6c325b20955de1efa1
studvaikmu5453 <mugesh.vaikundamani@stud.th-rosenheim.de>
2025-05-25 02:21:19 +0200

    update

--- Src/Model/falcon_r1/Scripts/falcon_r1_custom.py
+++ Src/Model/falcon_r1/Scripts/falcon_r1_custom.py
@@ -104,7 +104,7 @@ names: ['object']
         'yolo', 'task=detect', 'mode=train',
         'model=yolov8n.pt',
         f'data={os.path.join(root_dir, "data.yaml")}',
-        'epochs=100'
+        'epochs=1000'
     ]
     print("🚀 Starting YOLOv8 training...")
     try:
